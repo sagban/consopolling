73d98e4347133063f92158309f5e0c450e827d67
priyankjpi07-sopho <priyankjpi07@gmail.com>
2018-03-10 12:27:21 +0530

    login page styling | footer fix

--- conso/polls/static/js/script.js
+++ conso/polls/static/js/script.js
@@ -1,13 +1,5 @@
 $(".button-collapse").sideNav();
 
 function validateForm() {
-    var num = document.forms["login_form"]["mobile"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-    else if ((x < 7000000000) || (x > 9999999999))
-    {
-
-    }
+    /*needs to be updated*/
 }
